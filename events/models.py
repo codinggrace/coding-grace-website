12b675d482097cb9c90b34ab04e0671605364e3e
whykay <whykay@gmail.com>
2014-07-11 00:32:49 +0100

    added city to the string returned for the event object, handy for dropdown list of events when selecting one to associate with a news post.

--- events/models.py
+++ events/models.py
@@ -102,7 +102,7 @@ class Event(models.Model):
     slug = models.SlugField(help_text="E.G. this-this-an-event")
 
     def __str__(self):
-        return "{} - {}".format(self.title, self.start_datetime)
+        return "[{}] {} - {}".format(self.location.city, self.title, self.start_datetime)
 
     def current_event(self):
         return self.start_datetime >= timezone.now() - datetime.timedelta(days=1)
